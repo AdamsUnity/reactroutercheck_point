f49867f987486a45f20d8f65f0e391b8ac8bdbf8
Adams Unity <128307592+AdamsUnity@users.noreply.github.com>
2023-12-08 09:33:25 +0100

    added the  navBar to the home paage to be able to acess other pages of the app.

--- src/data.js
+++ src/data.js
@@ -11,8 +11,7 @@ const movieCollection = [
   {
     id: 2,
     title: "The Shawshank Redemption ",
-    description:
-      "A tale of hope and redemption in prison retired from his violent career after marrying the love of his life. Her sudden death leaves John in deep mourning. When sadistic mobster Iosef Tarasov (Alfie Allen) and his thugs steal John's prized car and kill the puppy that was a last gift from his wife, John unleashes the remorseless killing machine within and seeks vengeance. Meanwhile, Iosef's father (Michael Nyqvist) John's former colleague -- puts a huge bounty on John's head",
+    description: "A tale of hope and redemption in prison",
     postURL:
       "https://m.media-amazon.com/images/M/MV5BNDE3ODcxYzMtY2YzZC00NmNlLWJiNDMtZDViZWM2MzIxZDYwXkEyXkFqcGdeQXVyNjAwNDUxODI@._V1_.jpg",
     rating: 2,
@@ -21,8 +20,7 @@ const movieCollection = [
   {
     id: 3,
     title: "The Dark Knight",
-    description:
-      "Batman faces off against the Joker retired from his violent career after marrying the love of his life. Her sudden death leaves John in deep mourning. When sadistic mobster Iosef Tarasov (Alfie Allen) and his thugs steal John's prized car and kill the puppy that was a last gift from his wife, John unleashes the remorseless killing machine within and seeks vengeance. Meanwhile, Iosef's father (Michael Nyqvist) John's former colleague -- puts a huge bounty on John's head",
+    description: "Batman faces off against the Joker",
     postURL:
       "https://m.media-amazon.com/images/I/91KkWf50SoL._AC_UF1000,1000_QL80_.jpg",
     rating: 3,
@@ -30,8 +28,7 @@ const movieCollection = [
   },
   {
     id: 4,
-    title:
-      "Pulp Fiction retired from his violent career after marrying the love of his life. Her sudden death leaves John in deep mourning. When sadistic mobster Iosef Tarasov (Alfie Allen) and his thugs steal John's prized car and kill the puppy that was a last gift from his wife, John unleashes the remorseless killing machine within and seeks vengeance. Meanwhile, Iosef's father (Michael Nyqvist) -- John's former colleague puts a huge bounty on John's head",
+    title: "Pulp Fiction",
     description: "A nonlinear narrative of crime and redemption",
     postURL:
       "https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p15684_v_v13_ak.jpg",
@@ -42,7 +39,7 @@ const movieCollection = [
     id: 5,
     title: "John Wick",
     description:
-      "Legendary assassin John Wick (Keanu Reeves) retired from his violent career after marrying the love of his life. Her sudden death leaves John in deep mourning. When sadistic mobster Iosef Tarasov (Alfie Allen) and his thugs steal John's prized car and kill the puppy that was a last gift from his wife, John unleashes the remorseless killing machine within and seeks vengeance. Meanwhile, Iosef's father (Michael Nyqvist) -- John's former colleague -- puts a huge bounty on John's head.",
+      "Legendary assassin John Wick (Keanu Reeves) retired from his violent career after marrying the love of his life. Her sudden death leaves John in deep mourning. When sadistic mobster Iosef Tarasov (Alfie Allen) and his thugs steal John's prized car and kill the puppy that was a last gift from his wife, John unleashes the remorseless killing machine within and seeks vengeance. Meanwhile, Iosef's father (Michael Nyqvist) -- John's former colleague  puts a huge bounty on John's head.",
     postURL:
       "https://m.media-amazon.com/images/M/MV5BMTU2NjA1ODgzMF5BMl5BanBnXkFtZTgwMTM2MTI4MjE@._V1_.jpg",
     rating: 4,
@@ -51,8 +48,7 @@ const movieCollection = [
   {
     id: 6,
     title: "Avengers",
-    description:
-      "A nonlinear narrative of fiction retired from his violent career after marrying the love of his life. Her sudden death leaves John in deep mourning. When sadistic mobster Iosef Tarasov (Alfie Allen) and his thugs steal John's prized car and kill the puppy that was a last gift from his wife, John unleashes the remorseless killing machine within and seeks vengeance. Meanwhile, Iosef's father (Michael Nyqvist) -- John's former colleague -- puts a huge bounty on John's head.",
+    description: "A nonlinear narrative of fiction",
     postURL:
       "https://cdn.vox-cdn.com/thumbor/3d5Enbpx7sMOp8cM7J4chyjk8SM=/1400x0/filters:no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/11614195/InfinityWar5aabd55fed5fa.jpg",
     rating: 5,
